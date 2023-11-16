b494f97f50366ca96f56525a90adbf0b6e743b11
viet <vietnguyenvan442@gmail.com>
2023-11-16 22:20:05 +0700

    update recomment

--- src/main/java/com/example/stepcounter/controller/RecommendationController.java
+++ src/main/java/com/example/stepcounter/controller/RecommendationController.java
@@ -2,13 +2,16 @@ package com.example.stepcounter.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.stepcounter.model.Recommendation;
 import com.example.stepcounter.service.user.UserServiceImpl;
+import com.example.stepcounter.model.User;
 
 @RestController
 @RequestMapping("/api/recommend")
@@ -21,9 +24,9 @@ public class ReconmmendationController {
     
     //giảm cân
     @GetMapping("/WeightLoss")
-    public @ResponseBody WeightLoss(@RequestHeader("token") String token){
-    	User user = userServiceImpl.getUser(token);
-        return re.WeightLossAdvisor(user);
+    public @ResponseBody WeightLoss(@RequestHeader("token") String token, @RequestParam("reducedWeight") int reducedWeight, @RequestParam("durationInDays") int durationInDays){
+    	User user = UserServiceImpl.getUser(token);
+        return re.WeightLossAdvisor(user, reducedWeight, durationInDays);
     }
     
     //Giữ dáng
