41efea850109ab0bccc757eac65e473b2eeef882
viet <vietnguyenvan442@gmail.com>
2023-11-16 22:51:45 +0700

    update

--- src/main/java/com/example/stepcounter/controller/RecommendationController.java
+++ src/main/java/com/example/stepcounter/controller/RecommendationController.java
@@ -1,5 +1,7 @@
 package com.example.stepcounter.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.stepcounter.dto.user.UserOutputDto;
 import com.example.stepcounter.model.Recommendation;
 import com.example.stepcounter.service.user.UserServiceImpl;
-import com.example.stepcounter.model.User;
 
 @RestController
 @RequestMapping("/api/recommend")
@@ -24,15 +26,15 @@ public class ReconmmendationController {
     
     //giảm cân
     @GetMapping("/WeightLoss")
-    public @ResponseBody WeightLoss(@RequestHeader("token") String token, @RequestParam("reducedWeight") int reducedWeight, @RequestParam("durationInDays") int durationInDays){
-    	User user = UserServiceImpl.getUser(token);
+    public @ResponseBody List<Integer> WeightLoss(@RequestHeader("token") String token, @RequestParam("reducedWeight") int reducedWeight, @RequestParam("durationInDays") int durationInDays){
+    	UserOutputDto user = UserServiceImpl.getUser(token);
         return re.WeightLossAdvisor(user, reducedWeight, durationInDays);
     }
     
     //Giữ dáng
     @GetMapping("/WeightMaintenance")
-    public @ResponseBody WeightMaintenance(@RequestHeader("token") String token){
-    	User user = userServiceImpl.getUser(token);
+    public @ResponseBody int WeightMaintenance(@RequestHeader("token") String token){
+    	UserOutputDto user = userServiceImpl.getUser(token);
         return re.WeightMaintenanceAdvisor(user);
     }
 }
