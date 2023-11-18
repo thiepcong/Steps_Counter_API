66f24900e2e09b0d70476c75bf2aee1abf4fc5fc
vietnguyenvan442 <131009460+vietnguyenvan442@users.noreply.github.com>
2023-11-18 21:30:21 +0700

    Update

--- src/main/java/com/example/stepcounter/service/recommentdation/RecommendationService.java
+++ src/main/java/com/example/stepcounter/service/recommentdation/RecommendationService.java
@@ -17,39 +17,39 @@ public class RecommendationService {
 	public List<Integer> WeightLossAdvisor(UserOutputDto user, int reducedWeight, int durationInDays) {
 		List<Integer> wl = new ArrayList<>();
         // Tính toán các thông số
-        int BMR = calculateBMR(user);
-        int totalCaloriesToLose = calculateCaloriesToLoseWeight(reducedWeight, durationInDays, BMR);
-        int caloriesPerDay = totalCaloriesToLose / durationInDays;
+        double BMR = calculateBMR(user);
+        double totalCaloriesToLose = calculateCaloriesToLoseWeight(reducedWeight, durationInDays, BMR);
+        double caloriesPerDay = totalCaloriesToLose / durationInDays;
 
         // Tạo lộ trình giảm cân theo hàm sigmoid
         for (int day = 1; day <= durationInDays; day++) {
-            int caloriesBurned = (int) (sigmoid(day, durationInDays) * caloriesPerDay + (int)caloriesPerDay/2);
-            wl.add(caloriesBurned);
+            double caloriesBurned = (sigmoid(day, durationInDays) * caloriesPerDay + caloriesPerDay/2);
+            wl.add((int)caloriesBurned);
         }
         return wl;
     }
 
  // Tính BMR theo công thức Mifflin-St Jeor cho nam hoặc nữ
-    public static int calculateBMR(UserOutputDto user) {
+    public static double calculateBMR(UserOutputDto user) {
         if (user.getGender() == 0) {
-            return (int) (88.362 + (13.397 * user.getWeight()) + (4.799 * user.getHeight()) - (5.677 * user.getAge()));
+            return (88.362 + (13.397 * user.getWeight()) + (4.799 * user.getHeight()) - (5.677 * user.getAge()));
         } else {
-            return (int) (447.593 + (9.247 * user.getWeight()) + (3.098 * user.getHeight()) - (4.330 * user.getAge()));
+            return (447.593 + (9.247 * user.getWeight()) + (3.098 * user.getHeight()) - (4.330 * user.getAge()));
         }
     }
 
     // Hàm tính tổng calo cần để giảm cân
-    private static int calculateCaloriesToLoseWeight(int reducedWeight, int durationInDays, int BMR) {
+    private static double calculateCaloriesToLoseWeight(int reducedWeight, int durationInDays, double BMR) {
        return (BMR*durationInDays + reducedWeight*7700);
     }
 
     // Hàm sigmoid để tạo đường cong giảm cân
-    private static int sigmoid(int x, int duration) {
-        return (int) (1 / (1 + Math.exp(-0.1 * (x - duration / 2))));
+    private static double sigmoid(int x, int duration) {
+        return (1 / (1 + Math.exp(-0.1 * (x - duration / 2))));
     }
     
     //Giữ dáng
     public int WeightMaintenance(UserOutputDto user) {
-    	return calculateBMR(user);
+    	return (int)(calculateBMR(user));
     }
 }
